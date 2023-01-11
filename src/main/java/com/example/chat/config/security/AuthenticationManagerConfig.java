4e0426e7d69d75e0a42a6fc72038f38e5d67ffb1
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-01-11 18:21:00 +0200

    Configure authentication manager

---
+++ src/main/java/com/example/chat/config/security/AuthenticationManagerConfig.java
@@ -0,0 +1,15 @@
+package com.example.chat.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
+@Configuration
+public class AuthenticationManagerConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+}
