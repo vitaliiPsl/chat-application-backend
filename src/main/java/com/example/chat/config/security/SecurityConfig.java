0bcf4c61a92c7ef077d3ea010db4969f671dad6a
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-01-11 18:41:26 +0200

    Create jwt token verification filter

--- src/main/java/com/example/chat/config/security/SecurityConfig.java
+++ src/main/java/com/example/chat/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.chat.config.security;
 
+import com.example.chat.config.security.jwt.JwtVerificationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -17,6 +19,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
+
+    private final JwtVerificationFilter jwtVerificationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -32,6 +36,8 @@ public class SecurityConfig {
                 .anyRequest().authenticated();
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtVerificationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
