803f4fe7473d6d5df3a016e83d6ded0a2f3759f5
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-01-12 15:56:50 +0200

    Override equals and hash code for MemberId

--- src/main/java/com/example/chat/model/chat/member/MemberId.java
+++ src/main/java/com/example/chat/model/chat/member/MemberId.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,4 +13,17 @@ import java.io.Serializable;
 public class MemberId implements Serializable {
     private String userId;
     private String chatId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MemberId memberId = (MemberId) o;
+        return Objects.equals(userId, memberId.userId) && Objects.equals(chatId, memberId.chatId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, chatId);
+    }
 }
