31468cb5f2a3ccff1b2b5d3be5f4093c2176ce94
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-01-13 11:21:13 +0200

    Add method that allows to search for users whose nickname contains provided value

--- src/main/java/com/example/chat/repository/UserRepository.java
+++ src/main/java/com/example/chat/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.chat.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByNickname(String nickname);
+
+    List<User> findByNicknameContainingIgnoreCase(String nickname);
 }
