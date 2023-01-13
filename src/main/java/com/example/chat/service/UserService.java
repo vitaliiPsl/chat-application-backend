a1ac1a98880ec014c59a8c1487f53e0bf92039fd
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-01-13 11:21:50 +0200

    Implement User service with methods for search by id and nickname

---
+++ src/main/java/com/example/chat/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.chat.service;
+
+import com.example.chat.payload.user.UserDto;
+
+import java.util.List;
+
+/**
+ * User service
+ *
+ * @see com.example.chat.model.user.User
+ */
+public interface UserService {
+
+    /**
+     * Get user by the give id
+     *
+     * @param userId id of the user
+     * @return retrieved user
+     */
+    UserDto getUser(String userId);
+
+    /**
+     * Get users by the given nickname
+     *
+     * @param nickname nickname of the user
+     * @return list of users with nickname that matches given one
+     */
+    List<UserDto> getUsersByNickname(String nickname);
+}
---
+++ src/main/java/com/example/chat/service/impl/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.chat.service.impl;
+
+import com.example.chat.exception.ResourceNotFoundException;
+import com.example.chat.model.user.User;
+import com.example.chat.payload.user.UserDto;
+import com.example.chat.repository.UserRepository;
+import com.example.chat.service.UserService;
+import com.example.chat.utils.PayloadMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final PayloadMapper mapper;
+
+    @Override
+    public UserDto getUser(String userId) {
+        log.debug("Get user by id {}", userId);
+
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isEmpty()) {
+            log.error("User with id {} doesn't exist", userId);
+            throw new ResourceNotFoundException(userId, User.class);
+        }
+
+        return mapper.mapUserToUserDto(user.get());
+    }
+
+    @Override
+    public List<UserDto> getUsersByNickname(String nickname) {
+        log.debug("Get users with nickname that matches '{}'", nickname);
+
+        List<User> users = userRepository.findByNicknameContainingIgnoreCase(nickname);
+
+        return users.stream().map(mapper::mapUserToUserDto).collect(Collectors.toList());
+    }
+}
